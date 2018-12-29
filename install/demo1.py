6f3c378dbcaa95e3581dfe823295b748eb868f7b
wnhwylyyyjiujiu <wnhwylyyyjiujiu@163.com>
2018-12-29 17:09:09 +0800

    Initial commit

---
+++ install/demo1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @time     : 2018/12/29 14:25
+# @Author   : CGJ
+# @File     :demo1.py
+import sys
+
+name = input("please input your name: ")
+print("hello {0}".format(name))
---
+++ install/demo2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @time     : 2018/12/29 15:58
+# @Author   : CGJ
+# @File     :demo2.py
+for i in range(1,10):
+    print(i)
