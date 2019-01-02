2e880c27f7409f3837e29825f34b964cb7da2db2
wnhwylyyyjiujiu <wnhwylyyyjiujiu@163.com>
2019-01-02 21:18:29 +0800

    a

--- install/demo3.py
+++ install/demo3.py
@@ -8,3 +8,21 @@ print(a)
 b = 3.1415926
 c = round(b,3)
 print(c)
+string = "   acbdefgsesadfsdxadfa   "
+result = string.find('s')
+print(result)
+print(string[0])
+# replace 替换
+print(string.replace('a',"AAA"))
+#split() 分隔符
+print(string.split('a'))
+#strip 去除字符串前后的字符
+print("my list :{0}".format(string))
+d = string.strip()
+#一定要用format这种格式
+print("my list :{0}".format(d))
+print("my list :{0}".format(d))
+print("my list :%s" %d)
+print("hello" + "world")
+
+
